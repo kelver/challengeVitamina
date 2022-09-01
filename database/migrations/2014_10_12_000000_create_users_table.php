e9950ac919353e4cb2a42c50df4084e87d7f3e7e
Kelver <kelver_kmv@hotmail.com>
2022-09-01 00:27:33 -0300

    chore: Add field type in user table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('token_verify');
-            $table->timestamp('token_validate')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
